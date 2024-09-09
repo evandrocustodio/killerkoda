d610c235fbba73a14471babb844948f4c1d5a7b9
Evandro Custodio <evandrocustodio@gmail.com>
2024-09-09 00:00:26 -0300

    Ajustando numero do exercicio

--- ci/foreground.sh
+++ ci/foreground.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
 fnm use --install-if-missing 20
-node -v # should print `v20.17.0`
-npm -v # should print `10.8.2`
+node -v 
+npm -v 
